1145ef81a4905ab38bcc4ba9b7e2d929ff21a766
Dell <Dell@Razan-Nasser>
2023-05-22 23:13:55 +0300

    Move from JUnit 3.8.1 to JUnit 4.13.2

--- test/studentgrade/StudentGradeTest.java
+++ test/studentgrade/StudentGradeTest.java
@@ -1,35 +1,38 @@
-package studentgrade;
+package StudentGrade;
 
-import junit.framework.TestCase;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
 
-/* JUnit 3.8.1 */
-public class StudentGradeTest extends TestCase {
+/* 
+ * Test class for Grade class.
+ */
+public class StudentGradeTest {
     
-    public StudentGradeTest(String testName) {
-        super(testName);
-    }
-
-    public void testGetGrade95() {
-        System.out.println("getGrade 95");
-        int mark = 95;
-        String expResult = "A";
-        String result = StudentGrade.getGrade(mark);
-        assertEquals(expResult, result);
-    }
-
-    public void testGetGrade85() {
-        System.out.println("getGrade 85");
-        int mark = 85;
-        String expResult = "B";
-        String result = StudentGrade.getGrade(mark);
-        assertEquals(expResult, result);
+    public StudentGradeTest() {
+        System.out.println("calling constructor()");
     }
     
-    public void testGetGrade75() {
-        System.out.println("getGrade 85");
-        int mark = 75;
-        String expResult = "F";
-        String result = StudentGrade.getGrade(mark);
-        assertEquals(expResult, result);
+    @BeforeClass
+    public static void setUpClass() {
+        System.out.println("calling setUpClass()");
     }
+    
+    @AfterClass
+    public static void tearDownClass() {
+         System.out.println("calling tearDownClass()");
+    }
+    
+    @Before
+    public void setUp() {
+         System.out.println("calling setUp()");
+    }
+    
+    @After
+    public void tearDown() {
+          System.out.println("calling tearDown()");
+    } 
 }
